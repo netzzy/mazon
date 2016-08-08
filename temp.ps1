d1bda25845a09d67495df154acad492529401df7
netzzy <evgeniy.netzz@gmail.com>
2016-08-08 20:58:34 +0300

    sdfsdf

--- temp.ps1
+++ temp.ps1
@@ -1,3 +1,1 @@
-aws.exe  autoscaling create-auto-scaling-group --auto-scaling-group-name cfg --launch-configuration-name cfg --min-size 1 --max-size 1 --desired-capacity 1 --default-cooldown 600 --termination-policies "OldestInstance" --availability-zones us-west-2a us-west-2b us-west-2c
-pause
-pause
+aws.exe  ec2 describe-spot-instance-requests 
